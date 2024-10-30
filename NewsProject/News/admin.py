0633c894fe6423f76bf2b61d440d1148735989da
Marina Tomenko <marina.tomenko@list.ru>
2024-10-30 16:40:26 +0300

    Добавление фото в админке

--- NewsProject/News/admin.py
+++ NewsProject/News/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
 from .models import News, Category
-
+from django.utils.safestring import mark_safe
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'category', 'title', 'content', 'created_at', 'photo', 'is_published')
+    list_display = ('id', 'category', 'title', 'content', 'created_at', 'get_photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_filter = ('is_published', 'id')
     list_editable = ['is_published', 'category']
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+        else:
+            return 'Фото нет'
+    get_photo_description = 'Миниатюра'
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     list_display_links = ('id', 'title')
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = 'Стрвница администратора'
+admin.site.site_header = 'Страница администратора'
